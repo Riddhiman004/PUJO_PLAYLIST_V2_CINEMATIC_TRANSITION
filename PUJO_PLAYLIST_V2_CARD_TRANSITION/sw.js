273ca613be55c6bdf7cbae4d49d5603701ebe2f3
Riddhiman Mukhopadhyay <rawinsane01@gmail.com>
2026-08-20 13:31:38 +0530

    Add PWA support and service worker

---
+++ PUJO_PLAYLIST_V2_CARD_TRANSITION/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "pujo-playlist-v1";
+
+const APP_FILES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/songs.js",
+  "/manifest.json",
+
+  "/icons/icon-192(1).png",
+  "/icons/icon-512(1).png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
